ee1838a32d8be0fc167fa21475310d6846782f74
Mauricio Espinosa <ing_mauricioespinosa@hotmail.com>
2024-06-13 21:46:37 -0600

    Plantilla clase 8

---
+++ clase8/PrimeraPracticaIntegradora/src/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+
+
+import viewsRouter from './routes/views.router.js';
+import __dirname from './utils.js';
+
+const app = express();
+
+const PORT = process.env.PORT||8080;
+
+const server = app.listen(PORT,()=>console.log(`Listening on ${PORT}`));
+
+app.engine('handlebars',handlebars.engine());
+app.set('views',`${__dirname}/views`);
+app.set('view engine','handlebars');
+
+app.use(express.static(`${__dirname}/public`));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.use('/',viewsRouter);
---
+++ clase8/PrimeraPracticaIntegradora/src/routes/views.router.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get('/',(req,res)=>{
+    res.render('Home');
+})
+
+
+export default router;
